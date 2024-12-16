05d62e3c616cdb36283d181968ff89ee065561af
Pranjal Gupta <109664273+72pranjal@users.noreply.github.com>
2024-12-16 14:29:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
   
-  
+   let table = document.querySelector('tbody')
+    let newRow =  document.createElement('tr')
+    table.prepend(newRow)
+    let cell1 = document.createElement('td')
+    cell1.textContent = 'New Cell1'
+    newRow.append(cell1)
+    let cell2 = document.createElement('td')
+    cell2.textContent = 'New Cell2'
+    newRow.append(cell2)
 }
